00e8036dc6201f2e53f409aa13f8793fb37a9331
Sakshi3636 <145250599+Sakshi3636@users.noreply.github.com>
2024-06-27 15:03:24 +0530

    Add files via upload

---
+++ 10 Code Onlineclass/Uppertolower.cs
@@ -0,0 +1,12 @@
+
+
+using System;
+public class hello{
+    public static void Main(string[] args){
+        string name ="Om";
+        
+        Console.WriteLine("UPPER CASE STRING IS : "+ name);
+        Console.WriteLine("LOWER CASE STRING IS : "+ name.ToLower());
+
+    }
+}
